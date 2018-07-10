d2dc2a89a305b8eeeec938b6a620a47bc4b2f24e
Paulo Henrique Rodrigues Pinheiro <paulohrpinheiro@gmail.com>
2018-07-09 22:26:47 -0300

    Fix mock path in patch

--- mocking/mod1/tests/test_mod1.py
+++ mocking/mod1/tests/test_mod1.py
@@ -5,7 +5,7 @@ from ..services import mod1_function
 
 
 class TestMod1(TestCase):
-    @patch('mod2.services.mod2_function', return_value='MOCKED!')
+    @patch('mod1.services.mod2_function', return_value='MOCKED!')
     def test_mod1(self, mock_mod2):
         expected = 'From _mod1_ I get this from _mod2_: [MOCKED!]'
         result = mod1_function()
