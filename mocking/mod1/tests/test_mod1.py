3b27217911ef8f921ebafa4bce8e016c215f5bc3
Paulo Henrique Rodrigues Pinheiro <paulohrpinheiro@gmail.com>
2018-07-09 22:16:10 -0300

    Mock mod2 in test

--- mocking/mod1/tests/test_mod1.py
+++ mocking/mod1/tests/test_mod1.py
@@ -1,11 +1,13 @@
 from unittest import TestCase
+from unittest.mock import patch
 
 from ..services import mod1_function
 
 
 class TestMod1(TestCase):
-    def test_mod1(self):
-        expected = 'I don\'t remember'
+    @patch('mod2.services.mod2_function', return_value='MOCKED!')
+    def test_mod1(self, mock_mod2):
+        expected = 'From _mod1_ I get this from _mod2_: [MOCKED!]'
         result = mod1_function()
 
         self.assertEqual(result, expected)
